f87740d0c4aa6dd9e731d0a187e07f6c91735609
haoming9245 <haoming9245@outlook.com>
2023-04-12 17:56:58 +0800

    Add files via upload

---
+++ hosts.py
@@ -0,0 +1,28 @@
+def cleanhosts(name):
+    with open('C:/Windows/System32/drivers/etc/hosts', 'r', encoding='utf-8') as f:
+        lines = f.readlines()
+    ge = [name, 'Beyond']
+    new = ''
+    for line in lines:
+        mode = True
+        for i in ge:
+            if i in line:
+                mode = False
+                break
+        if mode:
+            new += line
+    with open('C:/Windows/System32/drivers/etc/hosts', 'w', encoding='utf-8') as f:
+        f.write(new)
+
+
+if __name__ == '__main__':
+    cleanhosts('#discord')
+    cleanhosts('#github')
+    cleanhosts('#v2ex')
+    cleanhosts('#nyaa')
+    cleanhosts('#pixiv')
+    cleanhosts('#duckduckgo')
+    cleanhosts('#ms')
+    cleanhosts('#exhentai')
+    cleanhosts('#gravatar')
+    cleanhosts('#steam')
